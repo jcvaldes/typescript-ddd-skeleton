8b469955899284b8448f42aae30aac5177f087c9
Juan Carlos Valdés <idevkingos@gmail.com>
2022-05-27 01:07:26 -0300

    chore: base architecture

--- src/apps/mooc/backend/controllers/CoursesPutController.ts
+++ src/apps/mooc/backend/controllers/CoursesPutController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { CourseCreator } from '../../../../Contexts/Mooc/Courses/application/CourseCreator';
 import { Controller } from './Controller';
 import httpStatus from 'http-status';
-export default class CoursePutController implements Controller {
+export default class CoursesPutController implements Controller {
   constructor(private courseCreator: CourseCreator) {}
 
   async run(req: Request, res: Response) {
--- src/apps/mooc/backend/routes/courses.route.ts
+++ src/apps/mooc/backend/routes/courses.route.ts
@@ -1,8 +1,8 @@
 import { Router, Request, Response } from 'express';
 import container from '../dependency-injection';
-import CoursePutController from '../controllers/CoursePutController';
+import CoursesPutController from '../controllers/CoursesPutController';
 
 export const register = (router: Router) => {
-  const controller: CoursePutController = container.get('Apps.mooc.controllers.CoursePutController');
+  const controller: CoursesPutController = container.get('Apps.mooc.controllers.CoursesPutController');
   router.get('/courses/:id', (req: Request, res: Response) => controller.run(req, res));
 };
