bc8de21d4040c59d41da79f9f524af272044815e
Juan Carlos Valdés <idevkingos@gmail.com>
2022-05-27 00:21:47 -0300

    feat: base architecture

--- src/apps/mooc/backend/controllers/CoursePutController.ts
+++ src/apps/mooc/backend/controllers/CoursePutController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express';
-import httpStatus from 'http-status';
+import { CourseCreator } from '../../../../Contexts/Mooc/Courses/application/CourseCreator';
 import { Controller } from './Controller';
 
 export class CoursePutController implements Controller {
-  constructor() {}
+  constructor(private courseCreator: CourseCreator) {}
 
   async run(req: Request, res: Response) {
-    res.status(httpStatus.CREATED).send();
+    const { id, name, duration } = req.body;
+
+    await this.courseCreator.run(id, name, duration);
   }
 }
